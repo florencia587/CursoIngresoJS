c3ebfec4c820bcfd63c9043837632e0af5004f19
florencia587 <flopyescola@hotmail.com>
2017-02-01 12:03:38 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,15 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
-{
+{var nombre;  //definicion variable
+nombre= "florencia";
+nombre= prompt  ("ingrese su nombre" ,"natalia natalia");
+
+	alert("nombre " + nombre); 
+	 
+
+	//esto es un comentario de una linea 
+	var person = prompt("ingresa tu nombre", "natalia natalia");
 	
 }
 
