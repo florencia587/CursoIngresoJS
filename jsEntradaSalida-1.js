309228b8d05a9e7d887ca4ecb4b0ab126c97127f
florencia587 <flopyescola@hotmail.com>
2017-02-07 17:39:20 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,9 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+function Mostar()
+{alert("esto funciona de maravilla");
+
+
+
+	
+}
+
---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,16 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{var nombre;     //definicion variable
+nombre= "florencia"
+nombre= prompt ("ingrese su nombre","natalia natalia");
+var person = prompt("florencia escola");
+alert("nombre" + florencia);
+
+
+	 
+
+	
+	
+}
+
