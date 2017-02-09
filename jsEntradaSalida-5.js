a1847631c97cc4946d467f4a6384eae62f1a8ec1
florencia587 <flopyescola@hotmail.com>
2017-02-08 23:27:34 -0300

    Add files via upload

---
+++ jsEntradaSalida-5.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function Mostar()
+{	
+	var nombre;
+	var edad;
+
+	nombre=document.getElementById('elNombre').value;
+	nombre=document.getElementById('laEdad').value;
+
+	alert("usted se llama " + "jose"+ "tiene"+ 66 +"años");
+	
+}
+
