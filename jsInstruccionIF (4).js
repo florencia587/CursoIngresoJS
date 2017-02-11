4bd8d13f7511290b28677876ee41ac95fb345e26
florencia587 <flopyescola@hotmail.com>
2017-02-11 10:49:25 -0300

    Add files via upload

---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,20 @@
+function Mostrar()
+{
+//tomo la edad 
+var edad;
+edad=document.getElementById('edad').value;
+if(edad<18)
+{
+	if(edad>12)
+	{
+		alert("no es adolescente");
+
+	}
+	{
+		alert("adolescente");
+	}
+
+
+
+
+}//FIN DE LA FUNCIÓN
